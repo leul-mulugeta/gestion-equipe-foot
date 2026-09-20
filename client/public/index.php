840afb4f64f2fe94a9ad07badb9db355ad74d946
Leul Mulugeta <leulnebeyu22@gmail.com>
2026-09-20 16:09:22 +0200

    chore(client): squelette du service avec page de test

---
+++ client/public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../init.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8" />
+	<title>Gestion équipe de football</title>
+</head>
+
+<body>
+	<h1>Client Web</h1>
+	<p>Microservice client fonctionnel sur le port 8080.</p>
+</body>
+
+</html>
